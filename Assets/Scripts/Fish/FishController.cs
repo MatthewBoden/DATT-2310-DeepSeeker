5feae72855a5be3b94fb294aa86c35d5313753e5
BarbodH <habibi.k.barbod@gmail.com>
2025-02-23 01:42:23 -0500

    Polish player controller + move to dedicated namespace

--- Assets/Scripts/Fish/FishController.cs
+++ Assets/Scripts/Fish/FishController.cs
@@ -1,5 +1,6 @@
 using System;
 using Enums;
+using Player;
 using UnityEngine;
 
 namespace Fish
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,112 +1,123 @@
 using Interfaces;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    [Header("Movement Settings")]
-    public float moveSpeed = 1.5f;  // Movement speed
-    public float acceleration = 2f; 
-    public float sprintMultiplier = 1.75f; // When player presses shift, speed is multiplied by this value
-    public float health; // Health points
-    public float stamina; // How long player sprints for
-    public float strength; // How much damage player deals
-
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private Animator animator;
-    private SpriteRenderer spriteRenderer;
-    private bool isSprinting;
-    private bool isHurt;
-
-    [SerializeField] private LayerMask damageableLayer;
-    [SerializeField] private GameObject attackPosition;
-    [SerializeField] private Vector2 attackCapsuleSize;
-
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>(); // Get Rigidbody2D component
-        animator = GetComponent<Animator>(); // Get Animator component
-        spriteRenderer = GetComponent<SpriteRenderer>(); // Correctly assign SpriteRenderer
-    }
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 1.5f;
+        [SerializeField] private float acceleration = 2f;
+        [SerializeField] private float sprintMultiplier = 1.75f;
 
-    void Update()
-    {
-        // Get movement input
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        [Header("Stats")]
+        [SerializeField] private float health;
+        [SerializeField] private float stamina;
+        [SerializeField] private float strength;
 
-        moveInput = new Vector2(moveX, moveY).normalized;
+        [Header("Attack")]
+        [SerializeField] private LayerMask damageableLayer;
+        [SerializeField] private GameObject attackPosition;
+        [SerializeField] private Vector2 attackCapsuleSize;
 
-        if (Input.GetKeyDown(KeyCode.J)) animator.SetBool("IsAttacking", true);
-    }
+        // State properties
+        public bool IsSprinting { get; private set; }
+        public bool IsHurt { get; private set; }
+        public bool IsAttacking { get; private set; }
+        
+        // Animation parameters
+        private static readonly int AnimatorParamAttacking = Animator.StringToHash("IsAttacking");
+        private static readonly int AnimatorParamIsSprinting = Animator.StringToHash("IsSprinting");
+        private static readonly int AnimatorParamIsMoving = Animator.StringToHash("IsMoving");
+        private static readonly int AnimatorParamXVelocity = Animator.StringToHash("xVelocity");
+        private static readonly int AnimatorParamIsHurt = Animator.StringToHash("IsHurt");
+        
+        private Rigidbody2D _rigidbody;
+        private Vector2 _moveInput;
+        private Animator _animator;
 
-    void FixedUpdate()
-    {
-        // Determine movement speed
-        float currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
-
-        // Apply velocity for smooth movement
-        Vector2 targetVelocity = moveInput * currentSpeed;
-        rb.velocity = Vector2.Lerp(rb.velocity, targetVelocity, acceleration * Time.deltaTime);
-
-        // Check if player is sprinting
-        if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D)))
-            isSprinting = true;
-        else if (!Input.GetKey(KeyCode.LeftShift))
-            isSprinting = false;
-
-        animator.SetBool("IsSprinting", isSprinting);
-
-        // Check if moving and update animation
-        bool isMoving = rb.velocity.magnitude > 0.1f;
-        animator.SetBool("IsMoving", isMoving);
-
-        // Set animation speed based on movement
-        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
-
-        // Flip sprite based on movement direction
-        if (rb.velocity.x > 0.1f)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (rb.velocity.x < -0.1f)
-            transform.localScale = new Vector3(-1, 1, 1);
-    }
-
-    public void TakeDamage(float damage)
-    {
-        animator.SetBool("IsHurt", true);
-        CancelInvoke(nameof(ResetHurt));
-        Invoke(nameof(ResetHurt), 1f);
-    }
-
-    private void ResetHurt()
-    {
-        animator.SetBool("IsHurt", false);
-    }
-
-    private void StartAttack()
-    {
-        var damageables = Physics2D.OverlapCapsuleAll(
-            attackPosition.transform.position,
-            attackCapsuleSize,
-            CapsuleDirection2D.Horizontal,
-            0f,
-            damageableLayer);
-
-        foreach (var damageable in damageables)
+        private void Start()
         {
-            damageable.GetComponent<IDamageable>().Damage(5); // TODO: Set up appropriate impact values
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
-    }
 
-    private void EndAttack()
-    {
-        animator.SetBool("IsAttacking", false);
-    }
-    
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(attackPosition.transform.position, attackCapsuleSize);
+        private void Update()
+        {
+            var moveX = Input.GetAxisRaw("Horizontal");
+            var moveY = Input.GetAxisRaw("Vertical");
+            _moveInput = new Vector2(moveX, moveY).normalized;
+
+            if (Input.GetKeyDown(KeyCode.J)) _animator.SetBool(AnimatorParamAttacking, IsAttacking = true);
+        }
+
+        private void FixedUpdate()
+        {
+            var currentSpeed = IsSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
+            var targetVelocity = _moveInput * currentSpeed;
+            _rigidbody.velocity = Vector2.Lerp(_rigidbody.velocity, targetVelocity, acceleration * Time.deltaTime);
+
+            // Check if player is sprinting
+            if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A)) ||
+                (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D)))
+                IsSprinting = true;
+            else if (!Input.GetKey(KeyCode.LeftShift))
+                IsSprinting = false;
+
+            _animator.SetBool(AnimatorParamIsSprinting, IsSprinting);
+
+            // Check if moving and update animation
+            var isMoving = _rigidbody.velocity.magnitude > 0.1f;
+            _animator.SetBool(AnimatorParamIsMoving, isMoving);
+            _animator.SetFloat(AnimatorParamXVelocity, Mathf.Abs(_rigidbody.velocity.x));
+
+            // Flip game object based on movement direction
+            transform.localScale = _rigidbody.velocity.x switch
+            {
+                > 0.1f => new Vector3(1, 1, 1),
+                < -0.1f => new Vector3(-1, 1, 1),
+                _ => transform.localScale
+            };
+        }
+
+        public void TakeDamage(float impact)
+        {
+            _animator.SetBool(AnimatorParamIsHurt, IsHurt = true);
+            CancelInvoke(nameof(ResetHurt));
+            Invoke(nameof(ResetHurt), 1f);
+        }
+
+        private void ResetHurt()
+        {
+            _animator.SetBool(AnimatorParamIsHurt, IsHurt = false);
+        }
+
+        private void StartAttack()
+        {
+            IsAttacking = true;
+
+            var damageables = Physics2D.OverlapCapsuleAll(
+                attackPosition.transform.position,
+                attackCapsuleSize,
+                CapsuleDirection2D.Horizontal,
+                0f,
+                damageableLayer);
+
+            foreach (var damageable in damageables)
+            {
+                damageable.GetComponent<IDamageable>().Damage(5); // TODO: Set up appropriate impact values
+            }
+        }
+
+        private void EndAttack()
+        {
+            _animator.SetBool(AnimatorParamAttacking, IsAttacking = false);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(attackPosition.transform.position, attackCapsuleSize);
+        }
     }
 }
