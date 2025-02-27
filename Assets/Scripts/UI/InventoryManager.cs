1d3de0d8a6c6a28eff9f0f62c72319c35b17e413
Matthew Bodenstein <50034384+MatthewBoden@users.noreply.github.com>
2025-02-27 17:46:21 -0500

    spawning new item
    broken

---
+++ Assets/Scripts/UI/InventoryManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventorySlot[] inventorySlots;
+    public GameObject InventoryItemPrefab;
+    public void AddItem(Item item)
+    {
+        // Find empty Slot
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null) {
+                SpawnNewItem(item, slot);
+                return;
+            }
+            
+        }
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot) { 
+        GameObject newItemGo = Instantiate(InventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitializeItem(item);
+    }
+}
